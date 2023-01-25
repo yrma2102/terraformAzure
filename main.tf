00b7c3d08ebca1eadad0c195d75784a61200da18
yrma2102 <42274589+yrma2102@users.noreply.github.com>
2023-01-25 20:53:07 -0300

    creando cluster Kubernetes

--- main.tf
+++ main.tf
@@ -1,74 +1,77 @@
-#crear maquina virtual en la cual se pueda instalar jenkins mediante ansible
-#resource group
 resource "azurerm_resource_group" "rg-demo" {
-    name = var.name
-    location = var.location
+    name = var.rg_name
+    location = var.rg_location
+
     tags = {
-      "Sec" = 2
-      "Grupo" = 7
+      "Grupo" = var.rg_group
     }
 }
 
-resource "azurerm_public_ip" "pip-demo" {
-    name = "public-ip"
-    resource_group_name = azurerm_resource_group.rg-demo.name
-    location = azurerm_resource_group.rg-demo.location
-    allocation_method = "Static"
-    tags = {
-      "diplomado" = "sec2"
-      "Grupo" = 7
-    }
-}
-
-#virtual network
 resource "azurerm_virtual_network" "vnet-demo" {
-    name = "diploGrupo7-net"
-    address_space = [ "10.0.0.0/16" ]
+    name = var.vnet_name
+    address_space = var.vnet_address_space
     location = azurerm_resource_group.rg-demo.location
     resource_group_name = azurerm_resource_group.rg-demo.name
 }
 
-#subnet
 resource "azurerm_subnet" "subnet-demo" {
-    name = "internal"
+    name = var.subnet_name
     resource_group_name = azurerm_resource_group.rg-demo.name
     virtual_network_name = azurerm_virtual_network.vnet-demo.name
-    address_prefixes = [ "10.0.2.0/24" ]
+    address_prefixes = var.subnet_address_prefixes
 }
 
-resource "azurerm_network_interface" "netinter-demo" {
-    name = "networkinterface"
-    location = azurerm_resource_group.rg-demo.location
-    resource_group_name = azurerm_resource_group.rg-demo.name
-    ip_configuration {
-      name = "internal"
-      subnet_id = azurerm_subnet.subnet-demo.id
-      private_ip_address_allocation = "Dynamic"
-      public_ip_address_id = azurerm_public_ip.pip-demo.id
-    }
-}
-
-resource "azurerm_linux_virtual_machine" "vm-demo" {
-    name = "diplo-machine-grupo7"
+resource "azurerm_container_registry" "acr-demo" {
+    name = var.acr_name
     resource_group_name = azurerm_resource_group.rg-demo.name
     location = azurerm_resource_group.rg-demo.location
-    size = "Standard_B1s"
-    network_interface_ids = [ azurerm_network_interface.netinter-demo.id ]
-    os_disk {
-      caching = "ReadWrite"
-      storage_account_type = "Standard_LRS"
-    }
-
-    source_image_reference {
-      publisher = "Canonical"
-      offer = "UbuntuServer"
-      sku = "16.04-LTS"
-      version = "latest"
-    }
-
-    computer_name = "hostname"
-    admin_username = "DiplomadoGrupo7"
-    admin_password = "DiplomadoGrupo7$"
-    disable_password_authentication = false
-  
+    sku = var.acr_sku
+    admin_enabled = var.acr_admin_enabled
 }
+#crear cluster con version 1.24.3
+#Habilitar Rbac
+#autoescalado entre 1 a 3
+resource "azurerm_kubernetes_cluster" "aks-demo" {
+    name = var.aks_name
+    location = azurerm_resource_group.rg-demo.location
+    resource_group_name = azurerm_resource_group.rg-demo.name
+    dns_prefix = var.aks_dns_prefix
+    kubernetes_version = var.aks_kubernetes_version
+    role_based_access_control_enabled = var.aks_rbac_enabled
+
+    default_node_pool {
+      name = var.aks_np_name
+      node_count = var.aks_np_node_count
+      vm_size = var.aks_np_vm_size
+      vnet_subnet_id = azurerm_subnet.subnet-demo.id
+      enable_auto_scaling = var.aks_np_enabled_auto_scaling
+      max_count = var.aks_np_max_count
+      min_count = var.aks_np_min_count
+      max_pods = "80"
+    }
+
+    service_principal {
+      client_id = var.aks_sp_client_id
+      client_secret = var.aks_sp_client_secret
+    }
+#policiy
+#network
+  network_profile {
+    network_plugin = var.aks_net_plugin
+    network_policy = var.aks_net_policy
+  }
+}
+#agregar pool de nodos adicional con label:adicional
+resource "azurerm_kubernetes_cluster_node_pool" "aks-demo" {
+  name                  = "internal"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks-demo.id
+  vm_size               = var.aks_np_vm_size
+  node_count            = 1
+  max_pods = "80"
+
+  tags = {
+    Environment = "Production"
+
+  }
+}
+#especificar la cantidad de pod por nodo en 80
